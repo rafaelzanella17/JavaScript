b711e94c8b5b0fcbb6b59c4a90bc00323902317b
Rafael Zanella <rafaelzanella17@gmail.com>
2021-11-16 17:32:50 -0300

    console.log();

--- console.log();/script.js
+++ console.log();/script.js
@@ -1,19 +1,13 @@
-/*
-Ferramentas de desenvolvedor do navegador você visualiza a mensagem.
-*/
-console.log('Vai aparecer esse texto no "console" do navegador.');
-console.log('=====================================================');
-
-
-
-/*
-Escreva abaixo entre as aspas e veja o resultado no console do navegador.
-*/
-console.log('');
-
-
-
 /* Referências 
 https://www.w3schools.com/jsref/met_console_log.asp
 https://developer.mozilla.org/pt-BR/docs/Web/API/Console/log
+
+Ferramentas de desenvolvedor do navegador você visualiza a mensagem.
 */
+
+console.log('Vai aparecer esse texto no "console do navegador".');
+console.log('==================================================');
+
+
+
+
