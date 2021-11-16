94ac586abfb06f02792477531717ab869edcdeb6
Rafael Zanella <rafaelzanella17@gmail.com>
2021-11-15 22:15:44 -0300

    console.log();

---
+++ consoleLog/script.js
@@ -0,0 +1,20 @@
+/*
+console.log()
+Escrever no console do navegador em JavaScript.
+*/
+console.log('Vai aparecer esse texto no "console" do navegador.');
+console.log('=====================================================');
+
+
+
+/*
+Escreva abaixo entre as aspas e veja o resultado no console do navegador.
+*/
+console.log('');
+
+
+
+/* Referências 
+https://www.w3schools.com/jsref/met_console_log.asp
+https://developer.mozilla.org/pt-BR/docs/Web/API/Console/log
+*/
