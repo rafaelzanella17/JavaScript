406d39d16450e023da5677fd6984c4ea08711675
Rafael Zanella <rafaelzanella17@gmail.com>
2021-11-16 11:15:06 -0300

    Alteração

---
+++ innerHTML = ' ' ;/script.js
@@ -0,0 +1,12 @@
+/* Referências 
+https://developer.mozilla.org/pt-BR/docs/Web/API/Element/innerHTML
+https://www.w3schools.com/jsref/prop_html_innerhtml.asp
+
+innerHTML acesse as referências acima.
+*/
+
+function executa() {
+var z = document.querySelector('.rafael');
+z.innerHTML = 'Zanella';
+console.log('Yes, olhe esse texto no console no navegador!');
+}
