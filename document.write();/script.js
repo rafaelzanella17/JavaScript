68e7df1023eeed1c9b3f4385752cb4f71c8c2c88
Rafael Zanella <rafaelzanella17@gmail.com>
2021-11-16 10:24:09 -0300

    document.write();

---
+++ document.write();/script.js
@@ -0,0 +1,19 @@
+/* Referências
+https://developer.mozilla.org/pt-BR/docs/Web/API/Document/write
+https://www.w3schools.com/jsref/met_doc_write.asp
+
+Comando em JavaScript para escrever na página HTML
+*/
+
+
+document.write('Escreva esse texto na página HTML do navegador!');
+
+
+/* Dica 
+Para quebrar linha no JavaScript USE  \n  
+Para quebrar linha no HTML USE  </br>
+*/
+document.write('</br>-----------------------------------------------');
+document.write('</br>');
+document.write('Criado em 16 novembro 2021. <br>Nova Roma do Sul');
+document.write(', 10:52h');
